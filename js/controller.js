6a232ce55415707f193a88b4db2a6b5d76c3e223
Balaji Rahunathan <32451142+Balaji-Rahunathan@users.noreply.github.com>
2022-10-22 12:29:58 +0530

    responsive to hd screens

--- js/controller.js
+++ js/controller.js
@@ -12,3 +12,30 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+const scrollContainer = document.getElementById('main');
+const scrollTarget = document.getElementById('target');
+
+scrollContainer.addEventListener('wheel', (evt) => {
+  scrollTarget.scrollLeft += evt.deltaY;
+  // throttle(500,evt)
+});
+
+function throttle( timeFrame,evt) {
+  var lastTime = 0;
+  return function () {
+    var now = new Date();
+    if (now - lastTime >= timeFrame) {
+      scrollTarget.scrollLeft += evt.deltaY;
+      lastTime = now;
+    }
+  };
+}
+
+var multiply = document.getElementById("multiply");
+var i;
+
+for (i = 0; i < 30; i++) {
+  const clone = multiply.cloneNode(true);
+  document.getElementById("target").appendChild(clone);
+}
