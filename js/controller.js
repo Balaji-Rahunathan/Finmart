55c8d4cf0ae61e7d265f1af809e088a22464d7b4
Balaji Rahunathan <32451142+Balaji-Rahunathan@users.noreply.github.com>
2022-10-25 20:42:42 +0530

    added navbar

--- js/controller.js
+++ js/controller.js
@@ -21,7 +21,7 @@ scrollContainer.addEventListener('wheel', (evt) => {
   // throttle(500,evt)
 });
 
-function throttle( timeFrame,evt) {
+function throttle(timeFrame, evt) {
   var lastTime = 0;
   return function () {
     var now = new Date();
@@ -38,4 +38,26 @@ var i;
 for (i = 0; i < 30; i++) {
   const clone = multiply.cloneNode(true);
   document.getElementById("target").appendChild(clone);
+}
+
+let isNavbarOpen = false;
+
+function navbarEvent() {
+  let navbar = document.getElementById("navbar-container")
+  let navbarLogo = document.getElementById("navbar-logo")
+  let hamIcon = document.getElementById("ham-icon")
+  if (isNavbarOpen) {
+    isNavbarOpen = false
+    setTimeout(() => {
+      navbar.style.backgroundColor = "#faeee7"
+      navbarLogo.src = "../assets/icons/logo.png"
+      hamIcon.src = "../assets/icons/hamburger.svg"
+    }, 260)
+
+  } else {
+    isNavbarOpen = true
+    navbar.style.backgroundColor = "#0E3192"
+    navbarLogo.src = "../assets/icons/logo-light.svg"
+    hamIcon.src = "../assets/icons/close.svg"
+  }
 }
